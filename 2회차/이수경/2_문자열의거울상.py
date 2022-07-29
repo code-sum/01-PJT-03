8d66d2eaf311ce4d5c730b95df8d0b010ea8e4c1
code-sum <yisugyeong@gmail.com>
2022-07-29 17:36:27 +0900

    [2회차_이수경] 최종제출

--- 2회차/이수경/2_문자열의거울상.py
+++ 2회차/이수경/2_문자열의거울상.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_문자열의거울상.txt")
+
+# 전체 테스트 케이스의 수
+n = int(input())
+
+for _ in range(1, n+1):
+
+    # ‘b’, ‘d’, ‘p’, ‘q’로 이루어진 문자열을 리스트로 받음
+    word = list(input())
+
+    # 리스트의 요소들을 역순으로 배열
+    reverse = word[::-1]
+
+    # 각 요소들이 붙여서 출력될 수 있도록 .join() 활용
+    Reverse = ''.join(reverse)
+
+    print('#%d %s' % (_, Reverse))
